eb53287cd1e98953cc2b162592e2fca4550bc604
sxmber <williamsmd002@gmail.com>
2023-02-03 22:45:44 -0700

    added keyboard

--- script.js
+++ script.js
@@ -1,43 +1,54 @@
-framelist = [`
- +---+
+framelist = [` +---+
      |
      |
      |
-    ===`, `
- +---+
+    ===`, ` +---+
  0   |
      |
      |
-    ==='''`,
-    `
- +---+
+    ===`,
+    ` +---+
  0   |
  |   |
      |
     ===`,
-    `
- +---+
+    ` +---+
  0   |
- |\  |
+ |\\  |
      |
     ===`,
-    `
- +---+
+    ` +---+
  0   |
-/|\  |
+/|\\  |
      |
     ===`,
-    `
- +---+
+    ` +---+
  0   |
-/|\  |
+/|\\  |
 /    |
     ===`,
-    `
- +---+
+    ` +---+
  0   |
-/|\  |
-/ \  |
+/|\\  |
+/ \\  |
     ===`]
 
-    
+const post = document.getElementById('post')
+let wordList;
+fetch("words.json")
+    .then(response => response.json())
+    .then(json => {
+        wordList = json;
+        console.log(wordList);
+    });
+console.log(wordList);
+// post.textContent = framelist[1];
+function generateWord() {
+
+}
+
+window.onload = () => {
+
+}
+
+
