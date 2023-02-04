266aee6794dd61d225d210725a59a1b0de630aa0
sxmber <williamsmd002@gmail.com>
2023-02-03 23:33:37 -0700

    added random word generator

--- script.js
+++ script.js
@@ -32,23 +32,41 @@ framelist = [` +---+
 /|\\  |
 / \\  |
     ===`]
+wordList = [
+    'speed', 'old', 'electricity', 'tales',
+    'explain', 'exchange', 'magnet', 'force',
+    'west', 'combination', 'lead', 'principal',
+    'driven', 'team', 'four', 'represent',
+    'after', 'orange', 'castle', 'speak',
+    'slabs', 'report', 'lay', 'time',
+    'pot', 'failed', 'tell', 'layers',
+    'country', 'order', 'point', 'helpful',
+    'common', 'area', 'bowl', 'vowel',
+    'green', 'gate', 'tape', 'lie',
+    'ran', 'written', 'snake', 'could',
+    'goes', 'nails', 'stronger', 'balance',
+    'had', 'entire'
+]
 
 const post = document.getElementById('post')
-let wordList;
-fetch("words.json")
-    .then(response => response.json())
-    .then(json => {
-        wordList = json;
-        console.log(wordList);
-    });
-console.log(wordList);
-// post.textContent = framelist[1];
+const word = document.getElementById('word');
+
 function generateWord() {
-
+    //grab random word from wordList
+    randomWord = wordList[Math.floor(Math.random() * (50 - 0) + 0)];
+    console.log(randomWord);
+    for (i = 1; i <= randomWord.length; i++)
+        word.textContent += " _ ";
 }
-
 window.onload = () => {
-
+    generateWord();
 }
+
+// post.textContent = framelist[1];
+
+
+
+
+
 
 
